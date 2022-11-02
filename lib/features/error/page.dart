3f69ce5d1d4b985e0f901fe6009794943f31996c
bannzai <kingkong999yhirose@gmail.com>
2022-11-02 20:04:36 +0900

    add universal error page

---
+++ lib/features/error/page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:taion/style/button.dart';
+
+class ErrorPage extends StatelessWidget {
+  final Object error;
+  final VoidCallback reload;
+
+  const ErrorPage({Key? key, required this.error, required this.reload})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            const Spacer(),
+            Image.asset("images/computer_sagi_error.png"),
+            Text(error.toString()),
+            const SizedBox(height: 10),
+            PrimaryButton(
+                onPressed: () async {
+                  reload();
+                },
+                text: "再読み込み"),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
