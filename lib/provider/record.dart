81409c0955486966fe4fb3c6de9435ed4f78965d
bannzai <kingkong999yhirose@gmail.com>
2022-11-03 20:06:53 +0900

    Add record provider

---
+++ lib/provider/record.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod/riverpod.dart';
+
+import '../entity/record.codegen.dart';
+
+String _collectionPathBuilder({required String userID}) {
+  return "/users/$userID/records";
+}
+
+FromFirestore<Record> _fromFirestore() =>
+    (snapshot, _) => Record.fromJson(snapshot.data()!);
+ToFirestore<Record> _toFirestore() => (value, _) => value.toJson();
+
+class CreateRecord {
+  Future<void> call(
+    Record record, {
+    required String userID,
+  }) async {
+    await recordCollectionReference(userID: userID)
+        .doc()
+        .set(record, SetOptions(merge: true));
+  }
+}
+
+final createRecordProvider = Provider((ref) => CreateRecord());
+
+CollectionReference<Record> recordCollectionReference(
+        {required String userID}) =>
+    FirebaseFirestore.instance
+        .collection(_collectionPathBuilder(userID: userID))
+        .withConverter(
+          fromFirestore: _fromFirestore(),
+          toFirestore: _toFirestore(),
+        );
+
+final recordsStreamProvider = StreamProvider((ref) =>
+    recordCollectionReference(userID: FirebaseAuth.instance.currentUser!.uid)
+        .orderBy("createdDateTime", descending: true)
+        .snapshots()
+        .map((event) => event.docs.map((e) => e.data()).toList()));
