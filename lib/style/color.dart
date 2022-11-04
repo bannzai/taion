429467af3cf751ebea153ea05c4098a81de00b00
bannzai <kingkong999yhirose@gmail.com>
2022-11-04 17:44:41 +0900

    add color

--- lib/style/color.dart
+++ lib/style/color.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColor {
   static const Color primary = Colors.pink;
   static const Color danger = Colors.red;
+  static const Color textMain = Colors.black;
 }
