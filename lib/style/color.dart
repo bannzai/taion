36ec23555ebf52a7f2f621065ed775a8a51bcbf7
bannzai <kingkong999yhirose@gmail.com>
2022-11-06 12:29:34 +0900

    add color of secondary background

--- lib/style/color.dart
+++ lib/style/color.dart
@@ -4,4 +4,5 @@ class AppColor {
   static const Color primary = Colors.pink;
   static const Color danger = Colors.red;
   static const Color textMain = Colors.black;
+  static const Color secondaryBackground = Color(0xFFE8EBED);
 }
