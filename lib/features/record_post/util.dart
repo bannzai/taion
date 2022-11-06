d2284ca11b9b9866b518432da2a91f6ede3036a9
bannzai <kingkong999yhirose@gmail.com>
2022-11-06 12:29:10 +0900

    Add util.dart

---
+++ lib/features/record_post/util.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:taion/style/color.dart';
+
+const secitonTitle = TextStyle(
+  fontWeight: FontWeight.w300,
+  fontSize: 17,
+  color: AppColor.textMain,
+);
