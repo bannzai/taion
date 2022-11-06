136414180fd5b0c1cf37eae643ec6f99085dc97e
bannzai <kingkong999yhirose@gmail.com>
2022-11-06 12:29:17 +0900

    add tags

---
+++ lib/features/record_post/tags.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:taion/entity/record.codegen.dart';
+import 'package:taion/features/record_post/util.dart';
+import 'package:taion/style/color.dart';
+
+class RecordPostTags extends StatelessWidget {
+  final ValueNotifier<List<String>> tags;
+
+  const RecordPostTags({super.key, required this.tags});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text("体調詳細", style: secitonTitle),
+        const SizedBox(height: 8),
+        Wrap(
+          spacing: 10,
+          children: defaultTags.map((e) {
+            final isSelected = tags.value.contains(e);
+            return ChoiceChip(
+              label: Text(e),
+              labelStyle:
+                  TextStyle(color: isSelected ? Colors.white : Colors.grey),
+              disabledColor: AppColor.secondaryBackground,
+              selectedColor: AppColor.primary,
+              selected: isSelected,
+              onSelected: (selected) {
+                if (isSelected) {
+                  tags.value.remove(e);
+                } else {
+                  tags.value.add(e);
+                }
+              },
+            );
+          }).toList(),
+        ),
+      ],
+    );
+  }
+}
