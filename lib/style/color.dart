9dc5e8bf5cf6bedf93f4a32981d71af7d749f409
bannzai <kingkong999yhirose@gmail.com>
2022-11-07 21:29:18 +0900

    Fix primary color

--- lib/style/color.dart
+++ lib/style/color.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class AppColor {
-  static const Color primary = Colors.pink;
+abstract class AppColor {
+  static const Color primary = Colors.lightGreen;
   static const Color danger = Colors.red;
   static const Color textMain = Colors.black;
   static const Color secondaryBackground = Color(0xFFE8EBED);
