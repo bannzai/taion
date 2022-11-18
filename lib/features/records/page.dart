52a188910c1ee48d90c2a49172436b04666d9caf
bannzai <kingkong999yhirose@gmail.com>
2022-11-18 19:32:59 +0900

    Fix

--- lib/features/records/page.dart
+++ lib/features/records/page.dart
@@ -1,13 +1,11 @@
 import 'package:async_value_group/async_value_group.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:taion/entity/record.codegen.dart';
 import 'package:taion/entity/user.codegen.dart';
 import 'package:taion/features/error/page.dart';
-import 'package:taion/features/records/component.dart';
 import 'package:taion/features/records/empty.dart';
 import 'package:taion/provider/record.dart';
 import 'package:taion/provider/user.dart';
