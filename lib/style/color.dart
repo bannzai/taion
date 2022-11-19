58d35b9e35b3a192c348e9f651585fe2c34d0e5a
bannzai <kingkong999yhirose@gmail.com>
2022-11-19 10:36:31 +0900

    Fix danger color

--- lib/style/color.dart
+++ lib/style/color.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 abstract class AppColor {
   static const Color primary = Colors.lightGreen;
-  static const Color danger = Colors.red;
+  static const Color danger = Color(0xFFFF4B00);
   static const Color textMain = Colors.black;
   static const Color textLightGray = Color(0xFFE2E2E2);
   static const Color secondaryBackground = Color(0xFFE8EBED);
