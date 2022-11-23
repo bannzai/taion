98b1327335773dcc3339b83e7cdaba4e8342468f
bannzai <kingkong999yhirose@gmail.com>
2022-11-23 20:27:46 +0900

    Add Actor provider

---
+++ lib/provider/actor.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod/riverpod.dart';
+
+import '../entity/actor.codegen.dart';
+
+String _collectionPathBuilder({required String userID}) {
+  return "/users/$userID/actors";
+}
+
+FromFirestore<Actor> _fromFirestore() =>
+    (snapshot, _) => Actor.fromJson(snapshot.data()!).copyWith(id: snapshot.id);
+ToFirestore<Actor> _toFirestore() => (value, _) => value.toJson();
+
+CollectionReference<Actor> actorCollectionReference({required String userID}) =>
+    FirebaseFirestore.instance
+        .collection(_collectionPathBuilder(userID: userID))
+        .withConverter(
+          fromFirestore: _fromFirestore(),
+          toFirestore: _toFirestore(),
+        );
+
+final actorsProvider = StreamProvider((ref) =>
+    actorCollectionReference(userID: FirebaseAuth.instance.currentUser!.uid)
+        .orderBy("index", descending: false)
+        .snapshots()
+        .map((event) => event.docs.map((e) => e.data()).toList()));
+
+class SetActor {
+  Future<void> call(
+    Actor actor, {
+    required String userID,
+  }) async {
+    await actorCollectionReference(userID: userID)
+        .doc(actor.id)
+        .set(actor, SetOptions(merge: true));
+  }
+}
+
+final setActorProvider = Provider((ref) => SetActor());
+
+class DeleteActor {
+  Future<void> call(
+    Actor actor, {
+    required String userID,
+  }) async {
+    await actorCollectionReference(userID: userID).doc(actor.id).delete();
+  }
+}
+
+final deleteActorProvider = Provider((ref) => DeleteActor());
