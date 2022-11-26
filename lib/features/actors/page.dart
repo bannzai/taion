f387fb762d9d999c2076f918cd80b51a9079e538
bannzai <kingkong999yhirose@gmail.com>
2022-11-26 11:18:13 +0900

    Add color picker

--- lib/features/actors/page.dart
+++ lib/features/actors/page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:taion/components/keyboard/keyboard_toolbar.dart';
 import 'package:taion/entity/actor.codegen.dart';
 import 'package:taion/provider/actor.dart';
+import 'package:taion/provider/user.dart';
 
 class ActorsPage extends HookConsumerWidget {
   const ActorsPage({super.key});
@@ -58,21 +60,53 @@ class ActorsPage extends HookConsumerWidget {
   }
 }
 
-class ActorListItem extends StatelessWidget {
+class ActorListItem extends HookConsumerWidget {
   final Actor actor;
   const ActorListItem({super.key, required this.actor});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userID = ref.watch(mustUserIDProvider);
+    final setActor = ref.watch(setActorProvider);
+    final color = useState(Color(actor.colorHexCode));
+
     return Row(
       children: [
         SizedBox(
           height: 60,
           width: 60,
-          child: CircleAvatar(
-            radius: 30,
-            backgroundColor: Color(actor.colorHexCode),
-            child: Text(actor.iconChar),
+          child: GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (_) => AlertDialog(
+                  title: const Text('Pick a color!'),
+                  content: SingleChildScrollView(
+                    child: ColorPicker(
+                      pickerColor: color.value,
+                      onColorChanged: (value) => color.value = value,
+                    ),
+                  ),
+                  actions: <Widget>[
+                    ElevatedButton(
+                      child: const Text('閉じる'),
+                      onPressed: () async {
+                        setActor(
+                          actor.copyWith(colorHexCode: color.value.value),
+                          userID: userID,
+                        );
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+            child: CircleAvatar(
+              radius: 30,
+              backgroundColor: Color(actor.colorHexCode),
+              child: Text(actor.iconChar),
+            ),
           ),
         ),
         const SizedBox(width: 16),
